77b04fe7937d0d89bd28572afab0457587a4d9bc
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-29 00:22:17 -0400

    feat: Requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { About } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente /About', () => {
+  it('Testa se é exibido na tela um h2 com texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const pokedexInfos = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+
+    expect(pokedexInfos).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const textOne = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémon');
+    const textTwo = screen.getByText('One can filter Pokémon by type, and see more details for each one of them');
+
+    expect(textOne).toBeInTheDocument();
+    expect(textTwo).toBeInTheDocument();
+  });
+
+  it('testa se atributo src da imagem tem o atributo correspondente', () => {
+    renderWithRouter(<About />);
+
+    const image = screen.getByAltText('Pokédex');
+
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
