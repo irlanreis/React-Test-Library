379406d2ad92453d3b2d6f4fe96df0db4a063ede
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-11-29 00:42:20 -0400

    feat: Requisito 03

--- src/pages/FavoritePokemon.js
+++ src/pages/FavoritePokemon.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { pokemonType } from '../types';
-
 import './favorite-pokemon.css';
 import { Pokemon } from '../components';
 
--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente /FavoritePokemon', () => {
+  it('Testa se exibe a mensagem, "No favorite pokémon found" caso o componente esteje vazio', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const emptyText = 'No favorite Pokémon found';
+
+    expect(screen.getByText(emptyText)).toBeInTheDocument();
+  });
+});
